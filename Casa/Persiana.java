afd45d21b9908dd3e8d24db7632a5d8c95cc6283
renzo378 <69973902+renzo378@users.noreply.github.com>
2022-11-01 13:08:48 -0300

    Add files via upload

---
+++ Casa/Persiana.java
@@ -0,0 +1,20 @@
+package Casa.elementos;
+
+public class Persiana{
+
+private String estado;
+private String material;
+
+
+public void Subir(){
+int x;
+int e;
+int w;
+this.estado = "subida";
+}
+public void bajar(){
+
+int w;
+this.estado = "bajada";
+}
+}
